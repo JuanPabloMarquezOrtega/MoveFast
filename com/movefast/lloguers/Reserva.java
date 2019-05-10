08805aaec2504f49c6d115d9439dc88308da7814
McUsam <47571071+McUsam@users.noreply.github.com>
2019-05-10 15:42:52 +0200

    Add files via upload

--- com/movefast/lloguers/Reserva.java
+++ com/movefast/lloguers/Reserva.java
@@ -15,59 +15,66 @@ public class Reserva {
 	private double preu;
 	private String targeta;
 	private boolean pagat;
-	
-	
+
 	public Reserva(Client client, Vehicle vehicle, LocalDate dataReserva, LocalDate dataRecollida,
-			LocalDate dataEntrega, double preu, String targeta) {
+			LocalDate dataEntrega, double preu, String targeta) throws DataException {
 		super();
 		this.client = client;
 		this.vehicle = vehicle;
 		this.dataReserva = dataReserva;
 		this.dataRecollida = dataRecollida;
-		if(dataRecollida.isBefore(dataEntrega)|| dataRecollida==dataEntrega)
+		if (dataRecollida.isBefore(dataEntrega) || dataRecollida == dataEntrega)
 			this.dataEntrega = dataEntrega;
 		else
-			try {
-				throw new DataException("La data de recollida ha de ser igual o anterior a la data d'entrega! ");
-			} catch (DataException e) {
-				System.err.println(e.getMessage());
-			}
-		if("Leasing".equals(client.getClass()))
-			this.preu = preu-preu*0.1;
-		else 
-			this.preu=preu;
+			throw new DataException("La data de recollida ha de ser igual o anterior a la data d'entrega! ");
+
+		if ("Leasing".equals(client.getClass()))
+			this.preu = preu - preu * 0.1;
+		else
+			this.preu = preu;
 		this.targeta = targeta;
 	}
+
 	public Vehicle getVehicle() {
 		return vehicle;
 	}
+
 	public void setVehicle(Vehicle vehicle) {
 		this.vehicle = vehicle;
 	}
+
 	public LocalDate getDataRecollida() {
 		return dataRecollida;
 	}
+
 	public void setDataRecollida(LocalDate dataRecollida) {
 		this.dataRecollida = dataRecollida;
 	}
+
 	public LocalDate getDataEntrega() {
 		return dataEntrega;
 	}
+
 	public void setDataEntrega(LocalDate dataEntrega) {
 		this.dataEntrega = dataEntrega;
 	}
+
 	public double getPreu() {
 		return preu;
 	}
+
 	public void setPreu(double preu) {
 		this.preu = preu;
 	}
+
 	public Client getClient() {
 		return client;
 	}
+
 	public LocalDate getDataReserva() {
 		return dataReserva;
 	}
+
 	public String getTargeta() {
 		return targeta;
 	}
